6792e20f22e31194696428bdc79e9522f2eb2c05
Emily Ball <emily.ball@bsc.es>
2026-06-30 10:53:24 +0200

    Add relative humidity calculation

--- R/00_utils.R
+++ R/00_utils.R
@@ -6,15 +6,33 @@
 # Description: This script contains the functions that are used in the rest of
 # the workflow. It is sourced from different scripts in the pipeline.
 #
-# Author(s): Your name
-# Date created: YYYY-MM-DD
-# Last updated: YYYY-MM-DD
-# Environment: local/HPC/remote (hub)
+# Author(s): Emily Ball
+# Date created: 2026-04-13
+# Last updated: 2026-04-13
+# Environment: local
 # Requirements: Please see R version and dependencies in the repo README.
 # ------------------------------------------------------------------------------
 
 # Section 1 ----
 
-## Subsection 1.1 ----
+# Calculate relative humidity from temperature and dew point temperature
+rel_hum <- function(tas, tdps) {
+  # Check class type
+  if (!inherits(tas, "s2dv_cube") || !inherits(tdps, "s2dv_cube")){
+    stop("'c4h_rel_hum' only accepts s2dv_cube objects as input.")
+  }
 
-my_function <- function(x) print("hello world!")
+  # Define constants
+  b <- 17.625
+  c <- 243.04
+
+  # Initialize s2dv_cube for output
+  hurs <- tas
+
+  hurs$data <- 100 * exp(b * c * (tdps$data - tas$data) /
+                           ((c + tdps$data) * (c + tas$data)))
+  hurs$attrs$Variable$metadata[["rel_hum"]]$units <- "percent"
+
+  return(hurs)
+
+}
